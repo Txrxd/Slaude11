806c9db4dd1ae283c2c621146a4d3162441d4167
Txrxd <132458554+Txrxd@users.noreply.github.com>
2023-05-19 14:09:18 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,81 @@
+const config = {
+    TOKEN: "xoxc-5249247219142-5268492461601-5255723415443-cacdb61dac5ec174d933a766f3ea8156690565324574b1a64eb981be22cf0253",
+    COOKIE: "xoxd-BQq0nz6RcJcwIWHh%2FLfNrWxMoQUhRODwvAYdzP3Fxls9bmjS9Gv5It0qkgelYF0ICT52A4eoZ5shZjFXQD%2BquKOsf%2BURlMAeEE9uDKFs0svw5%2F4FTuqPJTloirtsJFkNgyhMusUgOrW848ldv08g15J8UCmu7GIXH77bClATHloDUsFaYV2YsdjYKw%3D%3D",
+    TEAM_ID: "WhyIm",
+    CHANNEL: "C057HRJVCD8",
+    CLAUDE_USER: "U0587GG0L0Y",
+
+    edit_msg_with_ping: true,
+
+    // if edit_msg_with_ping is false:
+    // (i.e.send new message with ping instead of editting the first one)
+    // The final message we send with an @Claude ping to trigger his response. You don't need to add the @Claude bit, Slaude does that automatically.
+    // This goes just after your Jailbreak, it goes into its own "Human:" chat reply, instead of appended to the last prompt like JB
+    // Can be whatever you want it to be but keep in mind that it might result in unpredictable responses.
+    // Anything we put here will eat into our max available tokens so keep it brief. Anything above ~12000 characters will get cut off by Slack.
+    PING_MESSAGE_PREFIX: ".\n\n�A",
+    PING_MESSAGE: "r�",
+
+    // if edit_msg_with_ping is true:
+    PING_PREFIX: "�A",
+    PING_SUFFIX: "r�\n\n\n",
+    // You can put many tokens in these if you want, in the limit, they'll be pushed out of context
+    ping_at_start_of_msg: true, // if false, the ping will be edited in to the end of your prompt+card, so it will be between it and [Start Chat]
+    // The "Claude" string is necessary to trigger Claude, but it reminds Claude of who it is
+    // You can muddle the string by, for example, adding 'A' to the end of the prefix, and 'r' to the start of the suffix
+    // Which makes the text AClauder, which has a different token breakdown than Claude (Cla+ude -> AC+l+auder)
+
+    // The weird � character is less supported, but the thought process was to make the thing seem like a leftover error and for the model to ignore it, "AClauder" doesn't make any sense after all. You can remove it to try it out or if you think anything weird is happening, but felt good to me.
+    // Either way I strongly suggest not using ":" as it confuses it with the usual H: and A:
+
+    // Claude specs:
+    // Human, Assistant, H, A
+    rename_roles: {
+        'system': 'H',
+        'user': 'H',
+        'assistant': 'A',
+        'example_user': 'H',
+        'example_assistant': 'A'
+    },
+
+    minimum_split_size: 500,
+    dont_send_user_role_if_first_message: true,
+
+    stop_message_when_string_is_found: [
+        "\nH: ",
+        "\nHuman: ",
+    ],
+
+
+    auto_swipe: true,
+    // Be careful with `auto_swipe_minimum_length`, as it will not allow short messages through, set it to 0 if this is undersirable
+    // 0 to disable
+    auto_swipe_minimum_length: 0,
+    // If enough words on the blacklist are contained in the response, auto retry
+    // 0 to disable
+    auto_swipe_blacklist_threshold: 2,
+    auto_swipe_blacklist: [
+        "ethical",
+        "unethical",
+        "guidelines?",
+        "harmful",
+        "illegal",
+        "(un)?comfortable",
+        "engage",
+        "generat(e|ing)",
+        "nonconsensual",
+        "I apologize",
+        "(unable to|not) provide",
+        "inappropriate",
+        "content",
+        "(unable to) continue",
+    ],
+    // wait before starting to send text, lest it be filtered
+    auto_swipe_prebuffer_length: 200,
+
+    reply_timeout_delay: 1 * 60 * 1000,
+
+    PORT: 5004,
+}
+
+export default config;
